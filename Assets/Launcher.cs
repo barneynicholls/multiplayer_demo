9b6268ec1928d9833de3f7e787cdb1301c76bf7a
barneynicholls <barney.nicholls@bluetoad.net>
2020-10-06 14:37:05 +0100

    first commit

---
+++ Assets/Launcher.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using System;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        Connect();
+    }
+
+    public void Connect()
+    {
+        PhotonNetwork.GameVersion = "0.0.0";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Join();
+        base.OnConnectedToMaster();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        StartGame();
+        base.OnJoinedRoom();
+    }
+
+    private void StartGame()
+    {
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            PhotonNetwork.LoadLevel("Level01");
+        }
+    }
+
+    private void Join()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Create();
+        base.OnJoinRandomFailed(returnCode, message);
+    }
+
+    private void Create()
+    {
+        PhotonNetwork.CreateRoom(string.Empty);
+    }
+}
