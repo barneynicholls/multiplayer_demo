f0279dceb862737c5082bac319a84529afa0d202
Barney Nicholls <barney.nicholls@bluetoad.net>
2020-11-09 22:33:13 +0000

    simplified movement camera

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviourPunCallbacks
+{
+    CharacterController controller;
+
+    public float speed = 12f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine == false)
+            return;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
