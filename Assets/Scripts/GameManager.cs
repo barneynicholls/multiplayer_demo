2ba2aeba529c03f3bac1e20061da06ca2064a5b0
Barney Nicholls <barney.nicholls@bluetoad.net>
2020-11-09 22:13:40 +0000

    tidied up and added prototype materials

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public Transform spawnPoint;
+
+    void Start()
+    {
+        PhotonNetwork.Instantiate(playerPrefab.name,
+                                  spawnPoint.position,
+                                  spawnPoint.rotation);
+    }
+}
---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,32 @@
+﻿using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = "0.0.0";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomRoom();
+        base.OnConnectedToMaster();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.LoadLevel("Level01");
+        }
+        base.OnJoinedRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom(string.Empty);
+        base.OnJoinRandomFailed(returnCode, message);
+    }
+}
